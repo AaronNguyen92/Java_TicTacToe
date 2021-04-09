aebdcf07b093538e098b4134fff2e0541a0ffba8
Nguyen Phat Dat <81404423+AaronNguyen92@users.noreply.github.com>
2021-04-09 10:06:52 +0700

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author DELL
- */
+//Tọa độ của nước đi
 public class Point {
     public int x, y;
 
