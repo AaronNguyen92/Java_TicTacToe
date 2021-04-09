a0c349a4207f85c2c61be92a039095cb62e0348b
Nguyen Phat Dat <81404423+AaronNguyen92@users.noreply.github.com>
2021-04-09 10:06:08 +0700

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author DELL
- */
+
 public class TicTacToe {
     public static final Random RANDOM = new Random();
     
